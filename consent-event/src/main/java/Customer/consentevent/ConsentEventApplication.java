39c3f7ef97d1f4b7832586d33ac314977f1f0ede
sumanth kollipara <sumanthkumar44@gmail.com>
2023-07-27 19:40:53 +0100

    Add files via upload
    initial commit

---
+++ consent-event/src/main/java/Customer/consentevent/ConsentEventApplication.java
@@ -0,0 +1,13 @@
+package Customer.consentevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsentEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsentEventApplication.class, args);
+	}
+
+}
---
+++ consent-event/src/test/java/Customer/consentevent/ConsentEventApplicationTests.java
@@ -0,0 +1,13 @@
+package Customer.consentevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsentEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
