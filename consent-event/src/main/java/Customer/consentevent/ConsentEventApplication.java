e35c3fa5e1ea6410bb6a4ebe0cc4a45d78175e49
sumanthkumar44 <sumanthkumar44@gmail.com>
2023-07-27 21:58:04 +0100

    updating

--- consent-event/src/main/java/Customer/consentevent/ConsentEventApplication.java
+++ consent-event/src/main/java/Customer/consentevent/ConsentEventApplication.java
@@ -2,9 +2,6 @@ package Customer.consentevent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ConsentEventApplication  {
@@ -13,13 +10,6 @@ public class ConsentEventApplication  {
 		SpringApplication.run(ConsentEventApplication.class, args);
 	}
 
-/*	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html")
-				.addResourceLocations("classpath:/META-INF/resources/");
 
-		registry.addResourceHandler("/webjars/**")
-				.addResourceLocations("classpath:/META-INF/resources/webjars/");
-	}*/
 
 }
--- consent-event/src/main/java/Customer/consentevent/Model/EventModel.java
+++ consent-event/src/main/java/Customer/consentevent/Model/EventModel.java
@@ -5,12 +5,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 public class EventModel {
+    @Id
     private String eventId;
     private String payload;
 }
