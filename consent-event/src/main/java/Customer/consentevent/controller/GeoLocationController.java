59229e58ef2338278e5262c88fd646ec3cb75bb6
sumanthkumar44 <sumanthkumar44@gmail.com>
2023-07-30 12:05:18 +0100

    complete module

--- consent-event/src/main/java/Customer/consentevent/controller/GeoLocationController.java
+++ consent-event/src/main/java/Customer/consentevent/controller/GeoLocationController.java
@@ -1,6 +1,8 @@
 package Customer.consentevent.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,11 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/geolocation")
 public class GeoLocationController {
-    @GetMapping(
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public String getGeoLocation(@RequestParam("latitude") String latitude, @RequestParam("longitude") String longitude)
+    @GetMapping
+    public ResponseEntity<String> getGeoLocation(@RequestParam("latitude") String latitude, @RequestParam("longitude") String longitude)
     {
-        return "EC2M 4AA";
+        if(latitude.equalsIgnoreCase("latitude1")&&longitude.equalsIgnoreCase("longitude1")) {
+            return new ResponseEntity<>("EC2M 4AA", HttpStatus.OK);
+        }
+        else if(latitude.equalsIgnoreCase("latitude1")&&longitude.equalsIgnoreCase("longitude1")) {
+            return new ResponseEntity<>("EH2 4JN", HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
     }
 }
---
+++ consent-event/src/main/java/Customer/consentevent/helper/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package Customer.consentevent.helper;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
